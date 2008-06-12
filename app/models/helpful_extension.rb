a2dc97e728c229e48297cc8a025408f91b683a87
Jim Gay <jim@saturnflyer.com>
2008-06-11 22:39:36 -0400

    removed unnecessary validations

--- app/models/helpful_extension.rb
+++ app/models/helpful_extension.rb
@@ -5,12 +5,6 @@ class HelpfulExtension < ActiveRecord::Base
   has_many :helps, :dependent => :delete_all
   
   validates_presence_of :name
-  validates_presence_of :author_name
-  validates_presence_of :author_contact
-  
-  # Register fails without this...?
-  validates_format_of :author_name, :with => //, :allow_blank => true
-  validates_format_of :author_contact, :with => //, :allow_blank => true
   
   # Provide these details for your extension:
   #  :name (required)
@@ -22,15 +16,14 @@ class HelpfulExtension < ActiveRecord::Base
     failed_atts = {}
     helper = self.find_or_create_by_name(details[:name])
     details.each do |method,value|
-      unless self.respond_to?(method)
+      unless self.method_defined?(method)
         failed_atts[method.to_sym] = value
-      else
-        helper[method.to_sym] = value
       end
     end
     raise HelpfulExtension::DetailsError, "Setting #{failed_atts.inspect} does not work with HelpfulExtension. Check your attributes and try again." unless failed_atts.blank?
+    helper.update_attributes(details)
     puts helper.inspect
-    helper.save
+    # helper.save
     helper
   end
   
