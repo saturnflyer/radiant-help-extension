47fb6b0ba8810c0c5f55d38b9d216abf19842e48
Jim Gay <jim@saturnflyer.com>
2008-06-11 23:42:03 -0400

    using acts_as_tree

--- app/models/help.rb
+++ app/models/help.rb
@@ -1,6 +1,6 @@
 class Help < ActiveRecord::Base
   belongs_to :helpful_extension
-  has_one :parent, :class_name => 'Help', :foreign_key => 'parent_id'
+  acts_as_tree
   
   validates_uniqueness_of :topic, :scope => [:helpful_extension_id, :parent_id]
 end
