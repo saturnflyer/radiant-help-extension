35080ce5a93d9b135b5027a685760f226090bed1
Jim Gay <jim@saturnflyer.com>
2008-06-13 21:45:43 -0400

    fixed header changes

--- app/helpers/admin/help_helper.rb
+++ app/helpers/admin/help_helper.rb
@@ -1,18 +1,17 @@
 module Admin::HelpHelper
-  def help_tree_for(tree, parent_id = nil)
-    return false if tree.blank?
-    head_n = 2 || head_n
+  def help_tree_for(tree, parent_id = nil, head_n = 2)
     head_n = 6 if head_n > 5
+    return false if tree.blank?
     ret = "\n<ul>"
     tree.each do |node|
       if node.parent_id == parent_id
-        ret += "\n\t<li>"
+        ret << "\n\t<li>"
         ret << content_tag("h#{head_n}",node.topic)
         ret << content_tag('p',node.description)
-        ret += help_tree_for(node.children, node.id) unless node.children.blank?
-        ret += "\t</li>\n"
+        ret << help_tree_for(node.children, node.id, head_n + 1) unless node.children.blank?
+        ret << "\t</li>\n"
       end
     end
-    ret += "</ul>\n"
+    ret << "</ul>\n"
   end
 end
