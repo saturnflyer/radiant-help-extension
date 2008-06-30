7c6b41e78a27d1a8fc7cfb8cb18986e42d9ebd36
Jim Gay <jim@saturnflyer.com>
2008-06-30 14:28:51 -0400

    added controller specs

--- app/controllers/admin/help_controller.rb
+++ app/controllers/admin/help_controller.rb
@@ -2,12 +2,12 @@ class Admin::HelpController < ApplicationController
   include ActiveSupport::CoreExtensions::String::Inflections
   
   def index
-    @role = params[:role].nil? ? 'all' : params[:role]
+    @role = 'all'
     @docs = HelpDoc.find_for(:all)
     @file_not_found_page = Page.find(:first, :conditions => {:class_name => 'FileNotFoundPage'})
     @layouts = Layout.find(:all)
     @filters = TextFilter.descendants.uniq
-    @cms_name ||= Radiant::Config['admin.title']
+    @cms_name = Radiant::Config['admin.title']
   end
   
   def show
--- spec/controllers/admin/help_controller_spec.rb
+++ spec/controllers/admin/help_controller_spec.rb
@@ -1,5 +1,55 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe Admin::HelpController do
+  # use Radiant scenarios
+  scenario :users
   
+  before(:each) do
+    login_as :existing
+  end
+  
+  describe "calling index with GET" do
+    it "should render the index template" do
+      get :index
+      response.should render_template('admin/help/index')
+    end
+    
+    it "should assign HelpDocs for all users" do
+      @doc = "/path/to/doc/HELP.rdoc"
+      HelpDoc.stub!(:find_for).with(:all).and_return([@doc])
+      get :index
+      assigns[:docs].should == [@doc]
+    end
+    
+    it "should set the role to 'all' and assign it to the view" do
+      get :index
+      assigns[:role].should == 'all'
+    end
+    
+    it "should get the cms_name from Radiant::Config and assign it to the view" do
+      Radiant::Config.stub!(:[]).with('admin.title').and_return('Test CMS')
+      get :index
+      assigns[:cms_name].should == 'Test CMS'
+    end
+    
+    it "should find all TextFilters and assign them to the view" do
+      TextFilter.stub!(:descendants).and_return([MarkdownFilter, TextileFilter])
+      get :index
+      assigns[:filters].should == [MarkdownFilter, TextileFilter]
+    end
+    
+    it "should find all Layouts and assign them to the view" do
+      @layout1 = mock_model(Layout)
+      Layout.stub!(:find).with(:all).and_return([@layout1])
+      get :index
+      assigns[:layouts].should == [@layout1]
+    end
+    
+    it "should find the first FileNotFoundPage and assign it to the view" do
+      @page404 = mock_model(Page, :class_name => 'FileNotFoundPage') 
+      # Page.stub!(:find).with(:first).with(:conditions)#.and_return(@page404)
+      get :index
+      assigns[:file_not_found_page] == @page404
+    end
+  end
 end
